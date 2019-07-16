cf652c8370d6493ae5688a006c13ac42f11949fa
jonathan <jonathan@Zinui-MacBookPro.local>
2019-07-16 22:03:56 +0900

    사진 가져오기 기능 추가

--- backend/controller/index.js
+++ backend/controller/index.js
@@ -3,11 +3,13 @@ const post = require('./post');
 const comment = require('./comment');
 const like = require('./like');
 const marker = require('./marker');
+const picture = require('./picture');
 
 module.exports = {
     user,
     post,
     comment,
     like,
-    marker
+    marker,
+    picture
 };
---
+++ backend/controller/picture.js
@@ -0,0 +1,19 @@
+const knex = require('../knexfile');
+const path = require('path');
+const picturePath = path.join(__dirname, '../', '/public/uploads');
+
+module.exports = {
+    async get(params) {
+        const picture = await knex('picture')
+            .select('path')
+            .where('pictureId', params.pictureId)
+            .map((result) => {
+                return result.path;
+            });
+        if (!picture[0]) {
+            throw new Error("Picture doesn't exist");
+        } else {
+            return path.join(picturePath, picture[0]);
+        }
+    }
+};
