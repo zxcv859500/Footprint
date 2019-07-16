9fc2f28da654d1578ed9abaee4b6d515505f07bd
mon823 <33455696+mon823@users.noreply.github.com>
2019-07-16 14:10:01 +0900

    update NoticeBoardActivity

--- frontend/app/src/main/java/com/example/footprint/view/Activities/NoticeBoardActivity.java
+++ frontend/app/src/main/java/com/example/footprint/view/Activities/NoticeBoardActivity.java
@@ -6,6 +6,9 @@ import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.footprint.R;
 import com.example.footprint.view.Fragment.NoticeBoardBlueFragment;
@@ -17,6 +20,8 @@ public class NoticeBoardActivity extends AppCompatActivity {
     private Button btnNoticeRed, btnNoticeYellow, btnNoticeBlue;
 
 
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
     NoticeBoardRedFragment noticeBoardRedFragment;
     NoticeBoardYellowFragment noticeBoardYellowFragment;
     NoticeBoardBlueFragment noticeBoardBlueFragment;
@@ -30,6 +35,18 @@ public class NoticeBoardActivity extends AppCompatActivity {
         btnNoticeYellow = (Button)findViewById(R.id.btn_notice_yellow);
         btnNoticeBlue = (Button) findViewById(R.id.btn_notice_blue);
 
+        btnNoticeRed.setOnClickListener(new BtnOnClickListener());
+        btnNoticeYellow.setOnClickListener(new BtnOnClickListener());
+        btnNoticeBlue.setOnClickListener(new BtnOnClickListener());
+
+        noticeBoardRedFragment = new NoticeBoardRedFragment();
+        noticeBoardYellowFragment = new NoticeBoardYellowFragment();
+        noticeBoardBlueFragment = new NoticeBoardBlueFragment();
+
+        Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(tb) ;
+        setFragment(0);
+
 
     }
 
@@ -38,12 +55,36 @@ public class NoticeBoardActivity extends AppCompatActivity {
         public void onClick(View view) {
             switch (view.getId()){
                 case R.id.btn_notice_red:
+                    setFragment(0);
                     break;
                 case R.id.btn_notice_yellow:
+                    setFragment(1);
                     break;
                 case R.id.btn_notice_blue:
+                    setFragment(2);
                     break;
             }
         }
+    }
+
+    private void setFragment(int n){
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        switch (n){
+            case 0:
+                fragmentTransaction.replace(R.id.fragment,noticeBoardRedFragment);
+                fragmentTransaction.commit();
+                break;
+            case 1:
+                fragmentTransaction.replace(R.id.fragment,noticeBoardYellowFragment);
+                fragmentTransaction.commit();
+                break;
+            case 2:
+                fragmentTransaction.replace(R.id.fragment,noticeBoardBlueFragment);
+                fragmentTransaction.commit();
+                break;
+        }
+
     }
 }
