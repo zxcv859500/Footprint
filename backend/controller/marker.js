9ff18426e73a41757e193e13d78573aafb7158ae
jonathan <jonathan@Zinui-MacBookPro.local>
2019-07-16 21:50:32 +0900

    마커 타입 별로 가져오기 기능

--- backend/controller/marker.js
+++ backend/controller/marker.js
@@ -2,12 +2,34 @@ const knex = require('../knexfile');
 
 module.exports = {
     async list() {
-        const marker = knex('marker')
+        const marker = await knex('marker')
+            .distinct('type')
             .select('latitude', 'longitude')
+            .joinRaw('natural join markerApply natural join post')
             .map(r => ({
+                type: r.type,
                 latitude: r.latitude,
                 longitude: r.longitude
-            }))
-        return marker;
+            }));
+        let result = [];
+        marker.forEach((element) => {
+             const index = result.findIndex((res) => {
+                 return res.latitude === element.latitude && res.longitude === element.longitude
+             });
+             if (index === -1) {
+                 result.push({
+                     latitude: element.latitude,
+                     longitude: element.longitude,
+                     type: element.type
+                 })
+             } else if (result[index].type <= element.type) {
+                 result[index] = {
+                     latitude: element.latitude,
+                     longitude: element.longitude,
+                     type: element.type
+                 };
+             }
+        });
+        return result;
     }
 };
