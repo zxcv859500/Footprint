865f7999abb47cd248adeed28b72d1221e5ce166
zxcv859500 <zxcv859500@naver.com>
2019-07-22 07:40:06 +0900

    회원가입 예외처리 추가

--- backend/routes/api/auth/index.js
+++ backend/routes/api/auth/index.js
@@ -5,6 +5,12 @@ const auth = require('../../../middlewares/auth');
 router.post('/register', (req, res) => {
     const {username, password, nickname} = req.body;
 
+    if (!username || !password || !nickname) {
+        res.status(409).json({
+            error: "Username, password, nickname required"
+        })
+    }
+
     if (username.length <= 0 || username.length > 16 || password.length <= 0 || password.length > 16) {
         res.status(409).json({
             error: "username or password too long"
