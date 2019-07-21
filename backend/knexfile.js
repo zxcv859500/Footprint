01958e6019c627d2e288ab2737b502ce5397229f
zxcv859500 <zxcv859500@naver.com>
2019-07-22 06:03:39 +0900

    환경 변수 사용

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -3,9 +3,9 @@
 module.exports = require('knex') ({
   client: 'mysql2',
   connection: {
-    host: "172.19.0.2",
-    user: "root",
-    password: "rkfaorl453!",
-    database: "Footprint"
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
   }
 });
