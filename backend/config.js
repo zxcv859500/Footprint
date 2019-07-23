58bd06ca7511e8575b22863fffc2972342e9085a
zxcv859500 <zxcv859500@naver.com>
2019-07-23 18:02:36 +0900

    서비스 아이디 추가, api 키 변경

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     'secret': 'SeCrEtKeYfOrHaShInG',
     smsService: {
-        api: "ncp:sms:kr:256501093890:footprint",
+        api: "ZQa0Hi2MNrcJjmNjpGeo",
+        serviceId: "ncp:sms:kr:256501093890:footprint",
         secret: "a4e7f13ffa504f39bd4343672641bfbc"
     }
 };
