22bad3cc773622e6d690eec4af9eeb028a4bef17
zxcv859500 <zxcv859500@naver.com>
2019-07-23 17:46:55 +0900

    네이버 클라우드 플랫폼 api키 등록

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,7 @@
 module.exports = {
-    'secret': 'SeCrEtKeYfOrHaShInG'
+    'secret': 'SeCrEtKeYfOrHaShInG',
+    smsService: {
+        api: "ncp:sms:kr:256501093890:footprint",
+        secret: "a4e7f13ffa504f39bd4343672641bfbc"
+    }
 };
