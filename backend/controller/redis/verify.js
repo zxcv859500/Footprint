d49970f7c348ab2af0bfd362b9838b5ddc0942ba
zxcv859500 <zxcv859500@naver.com>
2019-07-26 12:06:32 +0900

    인증번호 생성 기능 추가

--- backend/controller/redis/verify.js
+++ backend/controller/redis/verify.js
@@ -1,2 +1,13 @@
 const client = require('../redisfile');
 
+module.exports = {
+  create(phone) {
+    function randomRange(n1, n2) {
+      return Math.floor( (Math.random() * (n2 - n1 + 1)) + n1 );
+    }
+    const verificationNumber = randomRange(10000, 99999);
+    client.set(phone, verificationNumber, 'EX', 3 * 60);
+
+    return verificationNumber;
+  }
+};
--- backend/controller/redisfile.js
+++ backend/controller/redisfile.js
@@ -1,4 +1,4 @@
-const redis = require('redis').createClient();
+const redis = require('redis').createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
 
 redis.on('error', function (err) {
     console.log("Redis error : " + err);
