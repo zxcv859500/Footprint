fda3061b0f95c4d84dd4716405bdff5a40e9989c
zxcv859500 <zxcv859500@naver.com>
2019-08-17 04:25:00 +0000

    통계 컨트롤러 추가

---
+++ backend/controller/mysql/statistics.js
@@ -0,0 +1,46 @@
+const knex = require('../knexfile');
+
+module.exports = {
+	list() {
+		const roads = await knex('marker')
+			.distinct('road')
+			.map((result) => {
+				return result.road;
+			});
+
+		builder = []
+
+		for (road of roads) {
+			const typeA = await knex.count('markerId as cnt')
+				.from('marker')
+				.joinRaw('natural join post')
+				.where('type', 0)
+				.map((result) => {
+					return result.cnt;
+				});
+
+			const typeB = await knex.count('markerId as cnt')
+				.from('marker')
+				.joinRaw('natural join post')
+				.where('type', 1)
+				.map((result) => {
+					return result.cnt;
+				});
+
+			const typeC = await knex.count('markerId as cnt')
+				.from('marker')
+				.joinRaw('natural join post')
+				.where('type', 2)
+				.map((result) => {
+					return result.cnt;
+				});
+			builder.push({
+				road: road,
+				typeA: typeA[0],
+				typeB: typeB[0],
+				typeC: typeC[0]
+			});
+		}
+		return builder;
+	}
+}
