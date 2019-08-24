7cf92c01e113a5c3ea422fe9f49c5b1967ee8a1c
zxcv859500 <zxcv859500@naver.com>
2019-08-24 18:39:08 +0900

    fixed #5

--- backend/controller/mysql/statistics.js
+++ backend/controller/mysql/statistics.js
@@ -8,44 +8,60 @@ module.exports = {
 				return result.road;
 			});
 
-		builder = []
+		const builder = [];
 
-		for (road of roads) {
-			const typeA = await knex.count('markerId as cnt')
-				.from('marker')
-				.joinRaw('natural join markerApply')
-				.joinRaw('natural join post')
-				.where('type', 0)
-				.andWhere('road', road)
-				.map((result) => {
-					return result.cnt;
-				});
-
-			const typeB = await knex.count('markerId as cnt')
-				.from('marker')
-				.joinRaw('natural join markerApply')
-				.joinRaw('natural join post')
-				.where('type', 1)
-				.andWhere('road', road)
-				.map((result) => {
-					return result.cnt;
-				});
-
-			const typeC = await knex.count('markerId as cnt')
-				.from('marker')
-				.joinRaw('natural join markerApply')
-				.joinRaw('natural join post')
-				.where('type', 2)
-				.andWhere('road', road)
-				.map((result) => {
-					return result.cnt;
-				});
-			builder.push({
+		for (const road of roads) {
+			let data = {
 				road: road,
-				typeA: typeA[0],
-				typeB: typeB[0],
-				typeC: typeC[0]
-			});
+				typeA: 0,
+				typeB: 0,
+				typeC: 0
+			};
+			const markerIds = await knex.select("markerId")
+				.from('marker')
+				.where('road', road)
+				.map((result) => {
+					return result.markerId
+				});
+			for (const markerId of markerIds) {
+				const typeA = await knex.count('markerId as cnt')
+					.from('marker')
+					.joinRaw('natural join markerApply')
+					.joinRaw('natural join post')
+					.where('type', 0)
+					.andWhere('road', road)
+					.map((result) => {
+						return result.cnt;
+					});
+
+				const typeB = await knex.count('markerId as cnt')
+					.from('marker')
+					.joinRaw('natural join markerApply')
+					.joinRaw('natural join post')
+					.where('type', 1)
+					.andWhere('road', road)
+					.map((result) => {
+						return result.cnt;
+					});
+
+				const typeC = await knex.count('markerId as cnt')
+					.from('marker')
+					.joinRaw('natural join markerApply')
+					.joinRaw('natural join post')
+					.where('type', 2)
+					.andWhere('road', road)
+					.map((result) => {
+						return result.cnt;
+					});
+				if (typeC[0] >= 1) {
+					data.typeC += 1;
+				} else if (typeB[0] >= 1) {
+					data.typeB += 1;
+				} else {
+					data.typeA += 1;
+				}
+			}
+			builder.push(data);
 		}
 		return builder;
 	}
